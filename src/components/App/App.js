1653d9ec29aa65f7894773915863a88af8d2da21
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-10-27 00:00:36 +0300

    add promo section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="app">
       <Header loggedIn={false} />
+      <Main />
       <Footer />
     </div>
   );
