a1157c62da63b148f4572b0035705eb1a9de1be8
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-10-29 23:05:43 +0300

    add movies route & component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,25 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
+import Movies from '../Movies/Movies';
 
-function App() {
+function App({ loggedIn }) {
   return (
     <div className="app">
-      <Header loggedIn={false} />
-      <Main />
+      <Header loggedIn={loggedIn} />
+      <Switch>
+        <Route path='/movies'>
+          <Movies />
+        </Route>
+        <Route exact path='/'>
+          <Main />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,10 @@
+import './Movies.css';
+
+function Movies() {
+  return (
+    <main className='movies'>
+    </main>
+  );
+};
+
+export default Movies;
