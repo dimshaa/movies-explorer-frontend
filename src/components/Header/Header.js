3087c5a45591992505e17d79d28a7a514dabc6e6
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-11-07 01:31:39 +0300

    fix header background color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../images/logo.svg';
 import BurgerMenu from '../BurgerMenu/BurgerMenu';
 import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
 function Header({ loggedIn }) {
+  const currentPath = useLocation();
+
   return (
-    <header className={`header ${!loggedIn && "header_unauthorized"}`}>
-      <Link to="/">
-        <img src={logo} className="logo" alt="логотип" />
+    <header className={`header ${currentPath.pathname === '/' && 'header_filled'}`}>
+      <Link to='/'>
+        <img src={logo} className='logo' alt='логотип' />
       </Link>
       <Navigation loggedIn={loggedIn} />
       {loggedIn && <BurgerMenu />}
