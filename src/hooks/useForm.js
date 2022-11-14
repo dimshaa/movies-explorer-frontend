091b2965913ac74f3e1b62e39872d1bcaee557da
Dmitry Shaposhnikov <ihatepost@gmail.com>
2022-11-14 23:38:58 +0300

    add useForm hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,26 @@
+import { useCallback, useState } from "react";
+
+export default function useForm() {
+  const [values, setValues] = useState({});
+  const [errors, setErrors] = useState({});
+  const [isValid, setIsValid] = useState(false);
+
+  const handleChange = (e) => {
+    const {value, name, validationMessage} = e.target;
+
+    setValues({...values, [name]: value});
+    setErrors({...errors, [name]: validationMessage});
+    setIsValid(e.target.closest('form').checkValidity())
+  };
+
+  const resetForm = useCallback(
+    (newValues = {}, newErrors = {}, newIsValid = false) => {
+      setValues(newValues);
+      setErrors(newErrors);
+      setIsValid(newIsValid);
+    },
+    [setValues, setErrors, setIsValid]
+  );
+
+  return {values, errors, isValid, handleChange, resetForm };
+}
