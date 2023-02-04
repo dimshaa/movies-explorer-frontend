2296b2797d75d7dd89b17920b0338b14e182d0f2
Dmitry Shaposhnikov <ihatepost@gmail.com>
2023-02-04 21:23:41 +0300

    fix api base url typo

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const MOVIES_API_URL = 'https://api.nomoreparties.co/';
-export const MAIN_API_URL = 'https://movies-explorer-api-psi.vercel.app/';
+export const MAIN_API_URL = 'https://movies-explorer-api-psi.vercel.app';
 
 export const EMAIL_REGEXP = '^[A-Za-z0-9.-_]+@[A-Za-z0-9.-]+[.][A-Za-z]{2,6}$';
 export const NAME_REGEXP = '^[а-яА-ЯёЁa-zA-Z-—\\s]+$';
